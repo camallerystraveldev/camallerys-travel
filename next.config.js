9743f1a044fa5f12c50721d021c40af4bd28b10e
Jorge Mederos Alvarado <camallerystraveldev@gmail.com>
2020-10-13 09:51:10 -0400

    add webpack file-loader

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const { config } = require("process");
+
+module.exports = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif)$/i,
+      use: [
+        {
+          loader: "file-loader",
+        },
+      ],
+    });
+
+    return config;
+  },
+};
