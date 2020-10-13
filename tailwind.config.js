aed64596970ab6ea68a861f19bb9614e9cb40aa9
Jorge Mederos Alvarado <camallerystraveldev@gmail.com>
2020-10-13 05:58:25 -0400

    add tailwindcss-filters

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,20 @@
+const twconfigFilters = {
+  theme: {
+    filter: {
+      // defaults to {}
+    },
+    backdropFilter: {
+      // defaults to {}
+      none: "none",
+      blur: "blur(2px)",
+    },
+  },
+  variants: {
+    filter: ["responsive"], // defaults to ['responsive']
+    backdropFilter: ["responsive"], // defaults to ['responsive']
+  },
+};
+
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
@@ -6,11 +23,14 @@ module.exports = {
   purge: ["./pages/**/*.tsx", "./src/**/*.tsx"],
   theme: {
     extend: {
+      ...twconfigFilters.theme,
       backgroundImage: (theme) => ({
         hero: "url('/assets/hero/hero.jpg')",
       }),
     },
   },
-  variants: {},
-  plugins: [],
+  variants: {
+    ...twconfigFilters.variants,
+  },
+  plugins: [require("tailwindcss-filters")],
 };
