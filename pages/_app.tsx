5cd71f0d72e0508e560e344d2acfad7884cb32f5
Jorge Mederos Alvarado <camallerystraveldev@gmail.com>
2020-10-16 18:29:00 -0400

    change _app style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import { AppProps } from "next/app";
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="min-h-screen min-w-full bg-gray-100 text-gray-900">
+    <div className="min-h-screen min-w-full bg-gray-100 text-gray-700 pb-10 overscroll-none">
       <Component {...pageProps} />
     </div>
   );
