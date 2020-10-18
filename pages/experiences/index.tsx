df815ecc69884ce50c72295a01f8833732657ee9
Jorge Mederos Alvarado <camallerystraveldev@gmail.com>
2020-10-18 02:33:50 -0400

    add experiences page prototype

--- pages/experiences/index.tsx
+++ pages/experiences/index.tsx
@@ -1,3 +1,22 @@
+import ExperienceCard from "../../components/experience-card";
+
 export default function Experiences() {
-  return <div id="experiences-page"></div>;
+  const experiences = [
+    { id: "habana-vieja", name: "Habana Vieja" },
+    { id: "valle-viñales", name: "Valle de Viñales" },
+    { id: "varadero", name: "Varadero" },
+  ];
+
+  return (
+    <div id="experiences-page">
+      <div className="p-2 pt-4 text-blue-800 font-extrabold text-3xl sm:text-4xl tracking-tight text-center leading-tight">
+        Experiencias en la isla
+      </div>
+      <div className="mt-8 px-5 flex flex-col md:flex-row items-center justify-start md:justify-center md:flex-wrap">
+        {experiences.map((e) => (
+          <ExperienceCard key={e.id} id={e.id} name={e.name} />
+        ))}
+      </div>
+    </div>
+  );
 }
